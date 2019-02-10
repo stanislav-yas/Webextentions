a1f637c2a70f9fd4ebfe7562b17acfdf2e1fe526
Stas <stanislav.yas@gmail.com>
2019-02-10 22:27:23 +0600

    chapter 9.3

--- LearnJS/js/myscript.js
+++ LearnJS/js/myscript.js
@@ -48,8 +48,20 @@ function printNode (node, nodeLevel){
     }
 }
 
-let el = document.querySelector("div.chapter");
-let clone = el.cloneNode(true);
-let before = document.querySelector("div.article");
-before.insertBefore(clone, null);
-printNode(document.body, 0);
+var clicks = 0;
+function handlerOne(e){
+
+    alert(e.type);
+}
+function handlerTwo(e){
+
+    clicks++;
+    var newNode = document.createElement("p");
+    newNode.textContent = "произошло нажатие " + clicks;
+    document.body.appendChild(newNode);
+}
+var rect = document.getElementById("rect");
+// прикрепляем первый обработчик
+rect.addEventListener("click", handlerOne);
+// прикрепляем второй обработчик
+rect.addEventListener("click", handlerTwo);
